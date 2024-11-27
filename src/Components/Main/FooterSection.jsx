7ea3bcb9d74cfe88d45e5f96d9221d98f26b3537
Pietroro98 <pietroro98@gmail.com>
2024-11-27 16:06:05 +0100

    aggiunta footer

--- src/Components/Main/FooterSection.jsx
+++ src/Components/Main/FooterSection.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./FooterSection.css";
-import Container from "react-bootstrap/Container"
+// import Container from "react-bootstrap/Container";
+ import { motion } from "framer-motion";
 
 function FooterSection() {
-  return(
-    <div className=""></div>
+  return (
+    <div className="py-5 ">
+        <motion.div
+        initial={{ opacity: 0, x: -300 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        
+        >
+
+        </motion.div>
+    </div>
   )
 }
 
--- src/Components/Main/ServiziCard.jsx
+++ src/Components/Main/ServiziCard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ServiziCard(props) {
