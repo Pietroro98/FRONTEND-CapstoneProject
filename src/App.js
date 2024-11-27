dc659c99ea165d6aa5f9beb34172ab56287c2957
Pietroro98 <pietroro98@gmail.com>
2024-11-27 15:29:39 +0100

    modifiche aggiuntive pagina servizi e stile

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import './App.css';
-import HeroSection from './Components/Hero/HeroSection';
-import AboutSection from './Components/Main/AboutSection';
-import ServiceSection from './Components/Main/ServiceSection';
-import MyNavBar from './Components/Mynav/MyNavBar';
-
+import "./App.css";
+import HeroSection from "./Components/Hero/HeroSection";
+import AboutSection from "./Components/Main/AboutSection";
+import ServiceSection from "./Components/Main/ServiceSection";
+import MyNavBar from "./Components/Mynav/MyNavBar";
+import FooterSection from "./Components/Main/FooterSection";
 function App() {
   return (
     <div className="App">
-      <MyNavBar/>
-      <HeroSection/>
-      <AboutSection/>
-      <ServiceSection/>
+      <MyNavBar />
+      <HeroSection />
+      <AboutSection />
+      <ServiceSection />
+      <FooterSection />
     </div>
   );
 }
---
+++ src/Components/Main/FooterSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./FooterSection.css";
+import Container from "react-bootstrap/Container"
+
+function FooterSection() {
+  return(
+    <div className=""></div>
+  )
+}
+
+export default FooterSection;
