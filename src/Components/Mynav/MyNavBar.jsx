141664efcc27cccac79215a128970796c64f1a99
Pietroro98 <pietroro98@gmail.com>
2024-11-28 16:13:36 +0100

    modifiche bordo navbar

--- src/Components/Mynav/MyNavBar.jsx
+++ src/Components/Mynav/MyNavBar.jsx
@@ -5,8 +5,13 @@ import { Container, Nav, Navbar } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
 import NavbarButton from "../Buttons/NavbarButton";
-
+import { useState } from "react";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 function MyNavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
   return (
     <Navbar expand="lg" className="position-absolute w-100 z-2">
       <Container>
@@ -25,28 +30,44 @@ function MyNavBar() {
           />
           Ghysa <span className="text-uppercase fw-bold text-purple">Gym</span>
         </motion.a>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="p-3 p-lg-0 mt-2 mt-lg-0">
+        <Navbar.Toggle
+          aria-controls="basic-navbar-nav"
+          onClick={toggleNavbar}
+          style={{ border: "none" }}
+        >
+          <FontAwesomeIcon
+            icon={isOpen ? faTimes : faBars}
+            style={{ fontSize: "1.5rem", color: "white" }}
+          />
+        </Navbar.Toggle>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="p-3 p-lg-0 mt-2 mt-lg-0"
+        >
           <motion.ul
-          className="navbar-nav me-auto justify-content-end w-100"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 1 }}
+            className="navbar-nav me-auto justify-content-end w-100"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
           >
-            <Nav.Link href="#"
-            className="text-light text-uppercase">Home</Nav.Link>
-            <Nav.Link href="#info"
-            className="text-light text-uppercase">Info</Nav.Link>
-            <Nav.Link href="#Servizi"
-            className="text-light text-uppercase">Servizi</Nav.Link>
-            <Nav.Link href="#Contatti"
-            className="text-light text-uppercase">Contatti</Nav.Link>
+            <Nav.Link href="#" className="text-light text-uppercase">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#info" className="text-light text-uppercase">
+              Info
+            </Nav.Link>
+            <Nav.Link href="#Servizi" className="text-light text-uppercase">
+              Servizi
+            </Nav.Link>
+            <Nav.Link href="#Contatti" className="text-light text-uppercase">
+              Contatti
+            </Nav.Link>
           </motion.ul>
           <motion.div
-          className="ms-0 ms-lg-2 mt-3 mt-lg-0"
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ duration: 1 }}
+            className="ms-0 ms-lg-2 mt-3 mt-lg-0"
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{ duration: 1 }}
           >
             <NavbarButton btnTitle="Inizia subito"></NavbarButton>
           </motion.div>
