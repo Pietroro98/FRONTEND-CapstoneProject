40086ab9e45a5119b146ade59f5ddb59ab0b07d6
Pietroro98 <pietroro98@gmail.com>
2024-12-04 18:05:51 +0100

    Aggiunta e recupero di schede di allenamento

--- src/Components/ExerciseComponent/WorkoutPlan.jsx
+++ src/Components/ExerciseComponent/WorkoutPlan.jsx
@@ -1,12 +1,24 @@
-import './WorkoutPlan.css';
-import React, { useState } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField, Button } from '@mui/material';
+import "./WorkoutPlan.css";
+import React, { useState, useEffect } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  TextField,
+  Button,
+} from "@mui/material";
+import { jwtDecode } from "jwt-decode";
 
 const WorkoutPlan = () => {
-  const [nomeScheda, setNomeScheda] = useState('');
-  const [dataCreazione, setDataCreazione] = useState('');
-  const [dataAllenamento, setDataAllenamento] = useState('');
-  const [errorMessage, setErrorMessage] = useState(''); // Stato per gestire eventuali errori
+  const [nomeScheda, setNomeScheda] = useState("");
+  const [dataCreazione, setDataCreazione] = useState("");
+  const [dataAllenamento, setDataAllenamento] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [workoutPlans, setWorkoutPlans] = useState([]);
 
   const handleCreateWorkoutPlan = async () => {
     const workoutData = {
@@ -15,43 +27,100 @@ const WorkoutPlan = () => {
       dataAllenamento,
     };
 
-    // Recupera il token dal localStorage
     const token = localStorage.getItem("authToken");
 
     if (!token) {
-      setErrorMessage("Token di autenticazione mancante. Assicurati di essere loggato.");
+      setErrorMessage(
+        "Token di autenticazione mancante. Assicurati di essere loggato."
+      );
       return;
     }
 
     try {
-      // Chiamata POST al backend per salvare la scheda di allenamento
-      const response = await fetch('http://localhost:3001/workout_plans', {
-        method: 'POST',
+      const decodedToken = jwtDecode(token);
+      console.log("Decoded token:", decodedToken);
+      const userId = decodedToken.sub;
+      if (!userId) {
+        setErrorMessage("ID utente non trovato nel token.");
+        return;
+      }
+      workoutData.userId = userId;
+
+      const response = await fetch("http://localhost:3001/workout_plans", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json', // Assicura che i dati vengano inviati come JSON
-          'Authorization': `Bearer ${token}`, // Aggiungi il token nell'header
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
         },
-        body: JSON.stringify(workoutData), // Converte il corpo della richiesta in JSON
+        body: JSON.stringify(workoutData),
       });
 
+      const data = await response.json();
+      console.log("Risposta dal server dopo creazione della scheda:", data);
+
       if (response.ok) {
-        const data = await response.json(); // Risposta del server in formato JSON
-        console.log("Scheda di allenamento creata con successo:", data);
-        // Reset dei campi
-        setNomeScheda('');
-        setDataCreazione('');
-        setDataAllenamento('');
-        setErrorMessage('');
+        fetchWorkoutPlans();
+        setNomeScheda("");
+        setDataCreazione("");
+        setDataAllenamento("");
+        setErrorMessage("");
       } else {
-        // Gestisci gli errori di risposta
         const errorData = await response.json();
-        setErrorMessage(errorData.message || "Errore nella creazione della scheda. Riprova.");
+        setErrorMessage(
+          errorData.message || "Errore nella creazione della scheda. Riprova."
+        );
       }
     } catch (error) {
       console.error("Errore nella creazione della scheda:", error);
       setErrorMessage("Errore nella creazione della scheda. Riprova.");
     }
   };
+
+  //GET Scheda allenamento
+  const fetchWorkoutPlans = async () => {
+    const token = localStorage.getItem("authToken");
+    if (!token) {
+      setErrorMessage(
+        "Token di autenticazione mancante. Assicurati di essere loggato."
+      );
+      return;
+    }
+
+    try {
+      const decodedToken = jwtDecode(token);
+      // console.log(decodedToken) // Ho verificato se Id utente mi veniva dato da userId o con altro nome, effettivamente e con sub
+      const userId = decodedToken.sub; // Otteniamo l'ID dell'utente dal token usando jwtdecode, Id utente si trova in sub
+      // console.log("User ID estratto dal token:", userId);
+
+      const response = await fetch(
+        `http://localhost:3001/workout_plans/user/${userId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      const data = await response.json();
+      // console.log("Schede ricevute dal backend:", data); //Schede che ricevo dal BE
+      
+      if (data.content && Array.isArray(data.content)) {
+        setWorkoutPlans(data.content);
+      } else {
+        setErrorMessage(
+          data.message || "Errore nel recupero delle schede. Riprova."
+        );
+      }
+    } catch (error) {
+      console.error("Errore nel recupero delle schede:", error);
+      setErrorMessage("Errore nel recupero delle schede. Riprova.");
+    }
+  };
+
+  useEffect(() => {
+    fetchWorkoutPlans();
+  }, []);
 
   return (
     <div className="workout-container">
@@ -90,8 +159,13 @@ const WorkoutPlan = () => {
             shrink: true,
           }}
         />
-        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>} {/* Mostra errore */}
-        <Button variant="contained" color="primary" onClick={handleCreateWorkoutPlan} className="create-button">
+        {errorMessage && <p style={{ color: "red" }}>{errorMessage}</p>}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={handleCreateWorkoutPlan}
+          className="create-button"
+        >
           Crea Scheda
         </Button>
       </div>
@@ -100,14 +174,25 @@ const WorkoutPlan = () => {
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Nome Esercizio</TableCell>
-              <TableCell>Serie</TableCell>
-              <TableCell>Ripetizione</TableCell>
-              <TableCell>Peso Usato</TableCell>
+              <TableCell>Nome Scheda</TableCell>
+              <TableCell>Data Creazione</TableCell>
+              <TableCell>Data Allenamento</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {/* Nessuna riga, tabella vuota */}
+            {Array.isArray(workoutPlans) && workoutPlans.length === 0 ? (
+              <TableRow>
+                <TableCell colSpan={3}>Nessuna scheda disponibile</TableCell>
+              </TableRow>
+            ) : (
+              workoutPlans.map((plan) => (
+                <TableRow key={plan.id}>
+                  <TableCell>{plan.nome_scheda}</TableCell>
+                  <TableCell>{plan.data_creazione}</TableCell>
+                  <TableCell>{plan.data_allenamento}</TableCell>
+                </TableRow>
+              ))
+            )}
           </TableBody>
         </Table>
       </TableContainer>
--- src/redux/reducers/userAuthSlice.js
+++ src/redux/reducers/userAuthSlice.js
@@ -10,11 +10,13 @@ const userAuthSlice = createSlice({
     name: 'userAuth',
     initialState,
     reducers: {
+        // Azione per il login dell'utente
         loginUser: (state, action) => {
             state.isAuthUser = true; // Imposta l'autenticazione a true
             state.userDetails = action.payload; // Salva le informazioni dell'utente
         },
         logoutUser: (state) => {
+            // Azione per il logout dell'utente
             state.isAuthUser = false; // Imposta l'autenticazione a false
             state.userDetails = null; // Rimuove le informazioni dell'utente
         },
