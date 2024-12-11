5532df2b8049db80e356409a72af4d3c90261777
Pietroro98 <pietroro98@gmail.com>
2024-12-11 15:01:45 +0100

    modifiche collegamenti logut account

--- src/Components/Account/AccountNavbar.jsx
+++ src/Components/Account/AccountNavbar.jsx
@@ -13,14 +13,12 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faDumbbell } from "@fortawesome/free-solid-svg-icons";
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { logoutU } from "../../redux/reducers/authSlice";
+import { Snackbar, Alert } from "@mui/material";
 
-const pages = [
-  "HomePage",
-
-];
+const pages = ["HomePage"];
 
 const settings = ["Login", "Logout"];
 
@@ -32,9 +30,9 @@ function AccountNavbar({ onPageChange }) {
   const dispatch = useDispatch();
 
   // snakbar
-  const [setOpen] = React.useState(false);
-  const [setMessage] = React.useState("");
-  const [setSeverity] = React.useState("");
+  const [message, setMessage] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [severity, setSeverity] = React.useState("");
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -52,7 +50,7 @@ function AccountNavbar({ onPageChange }) {
     setAnchorElUser(null);
   };
 
- // Logout
+  // Logout
   const handleLogout = () => {
     localStorage.removeItem("avatarURL");
     localStorage.removeItem("authToken");
@@ -64,19 +62,19 @@ function AccountNavbar({ onPageChange }) {
     setSeverity("success");
     setOpen(true);
     setTimeout(() => {
-      navigate("/register");
+      navigate("/login");
     }, 1500);
   };
 
- //navigazione navbar con pagine
+  //navigazione navbar con pagine
   const handleNavigate = (page) => {
     if (page === "Login") {
-        navigate("/login");
+      navigate("/login");
     } else if (page === "Logout") {
       handleLogout();
-    } else if (page === "Account"){
-        navigate("/account");
-    }else if (page === "HomePage") {
+    } else if (page === "Account") {
+      navigate("/account");
+    } else if (page === "HomePage") {
       navigate("/esercizi");
     } else {
       onPageChange(page);
@@ -85,63 +83,112 @@ function AccountNavbar({ onPageChange }) {
   };
 
   return (
+    <>
     <AppBar
-    position="fixed"
-    sx={{
-      backgroundColor: "#000000",
-      borderRadius: "20px",
-      margin: "10px",
-      width: "calc(100% - 20px)",
-    }}
-  >
-    <Container maxWidth="xl">
-      <Toolbar disableGutters>
-        <Typography
-          variant="h6"
-          noWrap
-          sx={{
-            mr: 2,
-            display: { xs: "none", md: "flex" },
-            fontFamily: "monospace",
-            fontWeight: 700,
-            letterSpacing: ".3rem",
-            color: "inherit",
-            textDecoration: "none",
-          }}
-        >
-          <FontAwesomeIcon
-            className="me-2"
-            icon={faDumbbell}
-            size="xl"
-            style={{ color: "#763abb" }}
-          />
-        </Typography>
+      position="fixed"
+      sx={{
+        backgroundColor: "#000000",
+        borderRadius: "20px",
+        margin: "10px",
+        width: "calc(100% - 20px)",
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{
+              mr: 2,
+              display: { xs: "none", md: "flex" },
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            <FontAwesomeIcon
+              className="me-2"
+              icon={faDumbbell}
+              size="xl"
+              style={{ color: "#763abb" }}
+            />
+          </Typography>
 
-        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-          <IconButton
-            size="large"
-            aria-label="account of current user"
-            aria-controls="menu-appbar"
-            aria-haspopup="true"
-            onClick={handleOpenNavMenu}
-            color="inherit"
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+              keepMounted
+              transformOrigin={{ vertical: "top", horizontal: "left" }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page}
+                  onClick={() => handleNavigate(page)}
+                  sx={{
+                    "&:hover": {
+                      color: "#763abb",
+                      fontWeight: "bold",
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  <Typography sx={{ textAlign: "center" }}>{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none",
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Menu
-            id="menu-appbar"
-            anchorEl={anchorElNav}
-            anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
-            keepMounted
-            transformOrigin={{ vertical: "top", horizontal: "left" }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-          >
+            Ghisa
+            <FontAwesomeIcon
+              className="me-2"
+              icon={faDumbbell}
+              size="xl"
+              style={{ color: "#763abb" }}
+            />
+            Gym
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
             {pages.map((page) => (
-              <MenuItem
+              <Button
                 key={page}
                 onClick={() => handleNavigate(page)}
                 sx={{
+                  my: 2,
+                  color: "white",
+                  display: "block",
                   "&:hover": {
                     color: "#763abb",
                     fontWeight: "bold",
@@ -149,97 +196,65 @@ function AccountNavbar({ onPageChange }) {
                   },
                 }}
               >
-                <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-              </MenuItem>
+                {page}
+              </Button>
             ))}
-          </Menu>
-        </Box>
+          </Box>
 
-        <Typography
-          variant="h5"
-          noWrap
-          component="a"
-          href="#app-bar-with-responsive-menu"
-          sx={{
-            mr: 2,
-            display: { xs: "flex", md: "none" },
-            flexGrow: 1,
-            fontFamily: "monospace",
-            fontWeight: 700,
-            letterSpacing: ".3rem",
-            color: "inherit",
-            textDecoration: "none",
-          }}
-        >
-          Ghisa
-          <FontAwesomeIcon
-            className="me-2"
-            icon={faDumbbell}
-            size="xl"
-            style={{ color: "#763abb" }}
-          />
-          Gym
-        </Typography>
-
-        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-          {pages.map((page) => (
-            <Button
-              key={page}
-              onClick={() => handleNavigate(page)}
-              sx={{
-                my: 2,
-                color: "white",
-                display: "block",
-                "&:hover": {
-                  color: "#763abb",
-                  fontWeight: "bold",
-                  backgroundColor: "transparent",
-                },
-              }}
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar src={avatarURL} />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
+              keepMounted
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
             >
-              {page}
-            </Button>
-          ))}
-        </Box>
-
-        <Box sx={{ flexGrow: 0 }}>
-          <Tooltip title="Open settings">
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar src={avatarURL} />
-            </IconButton>
-          </Tooltip>
-          <Menu
-            sx={{ mt: "45px" }}
-            id="menu-appbar"
-            anchorEl={anchorElUser}
-            anchorOrigin={{ vertical: "top", horizontal: "right" }}
-            keepMounted
-            transformOrigin={{ vertical: "top", horizontal: "right" }}
-            open={Boolean(anchorElUser)}
-            onClose={handleCloseUserMenu}
-          >
-            {settings.map((setting) => (
-              <MenuItem
-                key={setting}
-                onClick={() => handleNavigate(setting)}
-                sx={{
-                  "&:hover": {
-                    color: "#763abb",
-                    fontWeight: "bold",
-                    backgroundColor: "transparent",
-                  },
-                }}
-              >
-                <Typography sx={{ textAlign: "center" }}>
-                  {setting}
-                </Typography>
-              </MenuItem>
-            ))}
-          </Menu>
-        </Box>
-      </Toolbar>
-    </Container>
-  </AppBar>
+              {settings.map((setting) => (
+                <MenuItem
+                  key={setting}
+                  onClick={() => handleNavigate(setting)}
+                  sx={{
+                    "&:hover": {
+                      color: "#763abb",
+                      fontWeight: "bold",
+                      backgroundColor: "transparent",
+                    },
+                  }}
+                >
+                  <Typography sx={{ textAlign: "center" }}>
+                    {setting}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{vertical: "top", horizontal: "right" }}
+      >
+        <Alert
+          onClose={() => setOpen(false)}
+          severity={severity}
+          variant="filled"
+          sx={{ width: "100%" , backgroundColor: "#763abb", color: "#ffffff"}}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
 
