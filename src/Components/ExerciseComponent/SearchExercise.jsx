ae2d4e1438bb0cfe559dedf030eb8a7e64bd4fc1
Pietroro98 <pietroro98@gmail.com>
2024-12-13 11:45:20 +0100

    modifiche stile card cerca esercizi

--- src/Components/ExerciseComponent/SearchExercise.jsx
+++ src/Components/ExerciseComponent/SearchExercise.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import {
   Container,
   Box,
@@ -17,22 +17,22 @@ import {
   DialogTitle,
   DialogContent,
   DialogActions,
-} from '@mui/material';
+} from "@mui/material";
 import "./SearchExercise.css";
 
 function SearchExercise() {
   const [exercises, setExercises] = useState([]);
   const [bodyParts, setBodyParts] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [bodyPart, setBodyPart] = useState('');
-  const [selectedExercise, setSelectedExercise] = useState(null); 
+  const [searchQuery, setSearchQuery] = useState("");
+  const [bodyPart, setBodyPart] = useState("");
+  const [selectedExercise, setSelectedExercise] = useState(null);
   const [error, setError] = useState(false);
 
   // Fetch per ottenere le parti del corpo
   useEffect(() => {
     const fetchBodyParts = async () => {
       try {
-        const token = localStorage.getItem('authToken');
+        const token = localStorage.getItem("authToken");
         if (!token) {
           setError("Non sei autenticato. Effettua il login.");
           return;
@@ -64,7 +64,7 @@ function SearchExercise() {
   //fetch per ottenere gli esercizi con paginazione anche in base alla parte del corpo cercata
   const fetchExercises = async () => {
     try {
-      const token = localStorage.getItem('authToken');
+      const token = localStorage.getItem("authToken");
       if (!token) {
         setError("Non sei autenticato. Effettua il login.");
         return;
@@ -95,7 +95,7 @@ function SearchExercise() {
   };
 
   const filteredExercises = exercises.filter((exercise) =>
-    exercise.name.toLowerCase().includes(searchQuery.toLowerCase()) 
+    exercise.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   const handleOpenDialog = (exercise) => {
@@ -107,15 +107,15 @@ function SearchExercise() {
   };
 
   return (
-    <Container sx={{ mt: '100px' }}>
-      <Box sx={{ textAlign: 'center', my: 4 }}>
+    <Container sx={{ mt: "100px" }}>
+      <Box sx={{ textAlign: "center", my: 4 }}>
         <Typography
           variant="h3"
           component="h1"
           sx={{
-            fontWeight: 'bold',
-            '& span': {
-              color: '#5e2e8a',
+            fontWeight: "bold",
+            "& span": {
+              color: "#5e2e8a",
             },
           }}
         >
@@ -127,16 +127,18 @@ function SearchExercise() {
       <Box
         component="form"
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           gap: 2,
-          flexDirection: 'column',
+          flexDirection: "column",
           mt: 4,
         }}
       >
         <FormControl fullWidth sx={{ maxWidth: 600 }}>
-          <InputLabel id="body-part-label">Seleziona la parte del corpo</InputLabel>
+          <InputLabel id="body-part-label">
+            Seleziona la parte del corpo
+          </InputLabel>
           <Select
             labelId="body-part-label"
             value={bodyPart}
@@ -156,11 +158,11 @@ function SearchExercise() {
       <Box
         component="form"
         sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
           gap: 2,
-          flexDirection: 'column',
+          flexDirection: "column",
           mt: 4,
         }}
       >
@@ -174,18 +176,18 @@ function SearchExercise() {
         />
       </Box>
 
-      <Box sx={{ textAlign: 'center', mt: 4 }}>
+      <Box sx={{ textAlign: "center", mt: 4 }}>
         <Button
           variant="contained"
           color="primary"
-          onClick={fetchExercises} 
-          sx={{ backgroundColor: "#763abb"}}
+          onClick={fetchExercises}
+          sx={{ backgroundColor: "#763abb" }}
         >
           Cerca
-        </Button> 
+        </Button>
       </Box>
       {error && (
-        <Typography color="error" sx={{ textAlign: 'center', mt: 4 }}>
+        <Typography color="error" sx={{ textAlign: "center", mt: 4 }}>
           {error}
         </Typography>
       )}
@@ -193,31 +195,71 @@ function SearchExercise() {
       {/* Sezione di visualizzazione degli esercizi */}
       <Box
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fill, minmax(350px, 1fr))',
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(350px, 1fr))",
           gap: 4,
           mt: 4,
+          overflow: "visible",
+          padding:"15px",
+          
         }}
       >
         {filteredExercises.map((exercise) => (
-          <Card key={exercise.id} sx={{ maxWidth: 345, mx: 'auto' }}>
+          <Card
+            key={exercise.id}
+            sx={{
+              maxWidth: "345px",
+              margin: "0 auto",
+              borderRadius: "16px",
+              boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+              transition: "box-shadow 0.3s ease, transform 0.3s ease",
+              "&:hover": {
+                transform: "scale(1.05)",
+                boxShadow: "0 8px 20px rgba(255, 255, 255, 0.336)",
+              },
+            }}
+          >
             <CardMedia
               component="img"
               height="300"
               image={exercise.avatar}
               alt={exercise.name}
+              sx={{
+                borderTopLeftRadius: "16px",
+                borderTopRightRadius: "16px",
+                objectFit: "cover",
+              }}
             />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                sx={{
+                  fontWeight: "bold",
+                  color: "#fdfdfd",
+                  textAlign: "center",
+                  transition: "color 0.3s ease",
+                }}
+              >
                 {exercise.name}
               </Typography>
             </CardContent>
-            <CardActions>
+            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
               <Button
                 size="small"
                 color="primary"
                 onClick={() => handleOpenDialog(exercise)}
-                sx={{ color: "#763abb"}}
+                sx={{
+                  backgroundColor: "#763abb",
+                  color: "#ffffff",
+                  borderRadius: "2px",
+                  padding: "6px 16px",
+                  transition: "background-color 0.3s ease",
+                  "&:hover": {
+                    color: "#000000",
+                  },
+                }}
               >
                 Dettagli
               </Button>
@@ -228,24 +270,64 @@ function SearchExercise() {
 
       {/* Pop-up per i dettagli */}
       {selectedExercise && (
-        <Dialog open={Boolean(selectedExercise)} onClose={handleCloseDialog}>
-          <DialogTitle>{selectedExercise.name}</DialogTitle>
-          <DialogContent>
+        <Dialog
+          open={Boolean(selectedExercise)}
+          onClose={handleCloseDialog}
+          sx={{
+            borderRadius: "16px",
+            boxShadow: "0 4px 20px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <DialogTitle
+            sx={{
+              fontWeight: "bold",
+              color: "#ffffff",
+              fontSize: "1.5rem",
+              textAlign: "center",
+              paddingBottom: "10px",
+            }}
+          >
+            {selectedExercise.name}
+          </DialogTitle>
+          <DialogContent
+            sx={{
+              padding: "20px",
+              borderRadius: "12px",
+              marginBottom: "20px",
+            }}
+          >
             <Typography>
-              <strong className="strong-title">Attrezzatura:</strong> {selectedExercise.equipment}
+              <strong className="strong-title">Attrezzatura:</strong>
+              {selectedExercise.equipment}
             </Typography>
             <Typography>
-              <strong className="strong-title">Target:</strong> {selectedExercise.target}
+              <strong className="strong-title">Target:</strong>
+              {selectedExercise.target}
             </Typography>
             <Typography>
-              <strong className="strong-title">Muscoli secondari:</strong> {selectedExercise.secondaryMuscles}
+              <strong className="strong-title">Muscoli secondari:</strong>
+              {selectedExercise.secondaryMuscles}
             </Typography>
             <Typography>
-              <strong className="strong-title">Istruzioni:</strong> {selectedExercise.instructions}
+              <strong className="strong-title">Istruzioni:</strong>
+              {selectedExercise.instructions}
             </Typography>
           </DialogContent>
           <DialogActions>
-            <Button onClick={handleCloseDialog} sx={{  color: "#763abb"}}>
+            <Button
+              onClick={handleCloseDialog}
+              sx={{
+                padding: "8px 20px",
+                borderRadius: "5px",
+                fontWeight: "bold",
+                backgroundColor: "#763abb",
+                color: "#ffffff",
+                "&:hover": {
+                  backgroundColor: "#da3737",
+                  color: "#ffffff",
+                },
+              }}
+            >
               Chiudi
             </Button>
           </DialogActions>
